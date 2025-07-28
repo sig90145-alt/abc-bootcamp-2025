79a09431887fd612db283c3aa518f74e6548e409
SeongYeon Jang <sig90145@gmail.com>
2025-07-28 15:29:32 +0900

    implement number generation functions and example usage

---
+++ generator_01.py
@@ -0,0 +1,21 @@
+
+def generate_numbers_1(max_number: int):
+    print("called generate_numbers_1 funtion")
+    numbers = [] 
+    for i in range(1, max_number+1):
+        numbers.append(i)
+    return numbers
+
+# Generator 객체를 생성하는 함수
+def generate_numbers_2(max_number: int):
+    print("called generate_numbers_2 funtion")
+    for i in range(1, max_number+1):
+        yield i 
+
+
+
+numbers_1 = generate_numbers_1(10)
+numbers_2 = generate_numbers_2(10)
+print(numbers_1)
+print(numbers_2)
+
